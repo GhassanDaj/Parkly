b454a975a1c9500798c999fed3a288c52339dd7e
Ghassan AL Dajani <ghassandaj@gmail.com>
2024-09-30 21:38:08 -0400

    made some change to home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,64 +1,93 @@
 // screens/home_page.dart
 import 'package:flutter/material.dart';
-import 'package:parkly/screens/map_screen.dart';
-import 'package:parkly/screens/bookings_screen.dart';
-import 'package:parkly/screens/profile_screen.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:parkly/models/parking_spot.dart';
+import 'package:parkly/screens/parking_spot_details_screen.dart';
+import 'package:parkly/screens/booking_history_screen.dart';
+import 'package:parkly/services/auth_provider.dart';
+import 'package:parkly/services/booking_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:parkly/widgets/parking_spots_list.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  HomePageState createState() => HomePageState();
-}
-
-class HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-  static final List<Widget> _widgetOptions = <Widget>[
-    MapScreen(),
-    BookingsScreen(),
-    ProfileScreen(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  final List<String> _titles = ['Map', 'Bookings', 'Profile'];
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context, listen: false);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          _titles[_selectedIndex],
-          style: const TextStyle(color: Colors.white),
-        ),
+        title: Text('Available Parking Spots'),
         backgroundColor: Theme.of(context).primaryColor,
-        centerTitle: true,
       ),
-      body: _widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.mapMarkedAlt),
-            label: 'Map',
-          ),
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.calendarCheck),
-            label: 'Bookings',
-          ),
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.userCircle),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Theme.of(context).primaryColor,
-        unselectedItemColor: Colors.grey,
-        onTap: _onItemTapped,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              child: Text('Parkly',
+                  style: TextStyle(color: Colors.white, fontSize: 24)),
+              decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+            ),
+            ListTile(
+              leading: Icon(Icons.local_parking),
+              title: Text('Available Spots'),
+              onTap: () {
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.history),
+              title: Text('Your Bookings'),
+              onTap: () {
+                Navigator.pop(context); // Close the drawer
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => BookingHistoryScreen()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logout'),
+              onTap: () {
+                authProvider.signOut();
+                Navigator.popUntil(context, (route) => route.isFirst);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () => Provider.of<BookingProvider>(context, listen: false)
+            .fetchParkingSpots(),
+        child: Consumer<BookingProvider>(
+          builder: (context, bookingProvider, child) {
+            if (bookingProvider.isLoading) {
+              return Center(child: CircularProgressIndicator());
+            } else if (bookingProvider.errorMessage.isNotEmpty) {
+              return Center(
+                child: Text(
+                  bookingProvider.errorMessage,
+                  style: TextStyle(color: Colors.red, fontSize: 18),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            } else if (bookingProvider.parkingSpots.isEmpty) {
+              return Center(
+                child: Text(
+                  'No available parking spots at the moment.',
+                  style: TextStyle(color: Colors.grey, fontSize: 18),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            } else {
+              return ParkingSpotsList(
+                  parkingSpots: bookingProvider.parkingSpots);
+            }
+          },
+        ),
       ),
     );
   }
